b5de557a8eb16bb2562aba8f7fae56f4cee2e283
moksh jalendra <mokshjalendra33@gmail.com>
2026-01-09 01:17:41 +0530

    added new demo file

---
+++ newlec1.py
@@ -0,0 +1,14 @@
+
+
+import tkinter 
+root = tkinter.Tk()
+root.geometry('500x400')
+
+
+butoon1=tkinter.Button(text='butoon')
+butoon1.pack()
+
+entr=tkinter.Entry()
+entr.pack()
+
+root.mainloop()
